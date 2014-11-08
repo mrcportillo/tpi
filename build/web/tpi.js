c66fee677b09f419c72ec1c57170b6971eed4806
mrcportillo <marcossportillo@gmail.com>
2014-11-07 16:24:10 -0800

    Agregada nueva capa al vector de capas

--- build/web/tpi.js
+++ build/web/tpi.js
@@ -1,4 +1,4 @@
-    
+
       //diccionario de capas
     var capas = new Array();
     capas[1]='actividades_agropecuarias';
@@ -49,16 +49,16 @@
     capas[46]='veg_suelo_desnudo';
     capas[47]='vias_secundarias';
 
-    //funcion que devuelve un arreglo de capas 
-      
+    //funcion que devuelve un arreglo de capas
+
     var layer = new Array();
     function listarcapas(){
-        var i = 1; 
+        var i = 1;
         var capa = new ol.layer.Tile({
             title: "Global Imagery",
             source: new ol.source.TileWMS({
               url: 'http://maps.opengeo.org/geowebcache/service/wms',
-              params: { 
+              params: {
               LAYERS: 'bluemarble',
               VERSION: '1.1.1'
               }
@@ -68,24 +68,26 @@
         layer[0] = capa;
 
         for (i=1;i<=47;i++) {
-          var capa = new ol.layer.Image({  
+          var capa = new ol.layer.Image({
             visible: false,
             title: capas[i],
             source: new ol.source.ImageWMS({
             url: '/cgi-bin/qgis_mapserv.fcgi?map=/home/user/data/gisdatatpi/tpi.qgs',
-            
+
             //url: '/cgi-bin/qgis_mapserv.fcgi?map=/home/user/proyectoqgislaboratorioqgis.qgs',
             params: {LAYERS: capas[i]}
             })
           })
-          
+
       layer[i] = capa;
-     
+
       }
       return layer;
+
+
     }
-	// definicion del mapa 
-      
+	// definicion del mapa
+
     var map = new ol.Map({
         target: 'map',
         interactions:[],
@@ -101,13 +103,13 @@
     str1 = '<h3>Capas</h3><br/>';
     //generar el string con codigo html para definir la seccion de capas
     for (i=1;i<=47;i++) {
-        str1 = str1+'<input type="checkbox" id="check_layer_'+i+'"><label for="check_layer_'+i+'">'+capas[i]+'</label><br/>';    
-        
+        str1 = str1+'<input type="checkbox" id="check_layer_'+i+'"><label for="check_layer_'+i+'">'+capas[i]+'</label><br/>';
+
     }
     //insertar el string en el documento html
     node.innerHTML= str1;
     //arreglo de visibilidad
-    
+
     var visibilidad = new Array();
     for (i=1;i<=47;i++) {
         var visibilida = new ol.dom.Input(document.getElementById('check_layer_'+i+''));
@@ -115,144 +117,247 @@
         //(propiedad del input, objeto, propiedad del objeto)
         visibilida.bindTo('checked', map.getLayers().item(i), 'visible');
         visibilidad[i]=visibilida;
-        
+
+    }
+
+
+function medir(){
+    var source = new ol.source.Vector();
+    var vector = new ol.layer.Vector({
+        source: source,
+        style: new ol.style.Style({
+            fill: new ol.style.Fill({
+                color: 'rgba(255, 255, 255, 0.2)'
+            }),
+            stroke: new ol.style.Stroke({
+                color: '#ffcc33',
+                width: 2
+            }),
+            image: new ol.style.Circle({
+                radius: 7,
+                fill: new ol.style.Fill({
+                    color: '#ffcc33'
+                })
+            })
+        })
+    });
+    /**
+    * Currently drawed feature
+    * @type {ol.Feature}
+    */
+    var sketch;
+    /**
+    * Element for currently drawed feature
+     * @type {Element}
+                         */
+    var sketchElement;
+
+
+    /**
+     * handle pointer move
+     * @param {Event} evt
+     */
+    var mouseMoveHandler = function(evt) {
+      if (sketch) {
+        var output;
+        var geom = (sketch.getGeometry());
+
+          output = formatLength( /** @type {ol.geom.LineString} */ (geom));
+          console.log(output);
+
+        sketchElement.innerHTML = output;
+      }
+    };
+
+    //EL DRAMA ESTA ACA ABAJO. EL EVENTO MOUSEMOVE ES DE JQUERY. PROBABLEMENTE SI LO INCLUIS SOLUCIONAS
+    // PROBLEMA. AHI ANDA CON CADA CLICK.
+
+    map.addEventListener('click',mouseMoveHandler);
+    //map.addEventListener('mousemove', mouseMoveHandler);
+
+
+    //var typeSelect = document.getElementById('type');
+
+    var draw; // global so we can remove it later
+    function addInteraction() {
+        var type = "LineString";
+        draw = new ol.interaction.Draw({
+            source: source,
+            type: /** @type {ol.geom.GeometryType} */ (type)
+        });
+        map.addInteraction(draw);
+
+        draw.on('drawstart',
+        function(evt) {
+            //set sketch
+            sketch = evt.feature;
+            sketchElement = document.createElement('li');
+            var outputList = document.getElementById('measureOutput');
+            if (outputList.childNodes) {
+                outputList.insertBefore(sketchElement, outputList.firstChild);
+            } else {
+                outputList.appendChild(sketchElement);
+            }
+        }, this);
+
+        draw.on('drawend',
+            function(evt) {
+                // unset sketch
+                sketch = null;
+                sketchElement = null;
+            }, this);
+    } 
+    /**
+     * Let user change the geometry type.
+     * @param {Event} e Change event.
+     */
+    /*typeSelect.onchange = function(e) {
+      map.removeInteraction(draw);
+      addInteraction();
+    };*/
+
+
+    /**
+     * format length output
+     * @param {ol.geom.LineString} line
+     * @return {string}
+     */
+    var formatLength = function(line) {
+        var length = Math.round(line.getLength() * 150000);
+        //var length = Math.round(line.getLength() * 100) / 100;
+        var output;
+        if (length > 100) {
+            output = (Math.round(length / 1000 * 100) / 100) +
+            ' ' + 'km';
+        } else {
+            output = (Math.round(length * 100) / 100) +
+            ' ' + 'm';
+        }
+        return output;
+    };
+    addInteraction();
+
+};
+
+
+
+function agregarCapa(){
+    var source = new ol.source.Vector();
+    var vector = new ol.layer.Vector({
+        source: source,
+        style: new ol.style.Style({
+            fill: new ol.style.Fill({
+                color: 'rgba(255, 255, 255, 0.2)'
+            }),
+            stroke: new ol.style.Stroke({
+                color: '#ffcc33',
+                width: 2
+            }),
+            image: new ol.style.Circle({
+                radius: 7,
+                fill: new ol.style.Fill({
+                    color: '#ffcc33'
+                })
+            })
+        })
+    });
+
+
+    var draw; // global so we can remove it later
+    function addInteraction() {
+        var type = "Point";
+        draw = new ol.interaction.Draw({
+            source: source,
+            type: /** @type {ol.geom.GeometryType} */ (type)
+        });
+        map.addInteraction(draw);
+        draw.on('drawend',
+        function(evt) {
+            // unset sketch
+            wkt = 'POINT';
+        }, this);
     }
     
-    function medir(){
-
-            
-                var source = new ol.source.Vector();
-
-                        var vector = new ol.layer.Vector({
-                          source: source,
-                          style: new ol.style.Style({
-                            fill: new ol.style.Fill({
-                              color: 'rgba(255, 255, 255, 0.2)'
-                            }),
-                            stroke: new ol.style.Stroke({
-                              color: '#ffcc33',
-                              width: 2
-                            }),
-                            image: new ol.style.Circle({
-                              radius: 7,
-                              fill: new ol.style.Fill({
-                                color: '#ffcc33'
-                              })
-                            })
-                          })
-                        });
+    window.open('php/crearcapa.php?capanombre='+capanuevanombre);
+    var capa = new ol.layer.Image({
+        visible: true,
+        title: capanuevanombre,
+        source: new ol.source.ImageWMS({
+            url: '/cgi-bin/qgis_mapserv.fcgi?map=/home/user/data/gisdatatpi/tpi.qgs',
+            //url: '/cgi-bin/qgis_mapserv.fcgi?map=/home/user/proyectoqgislaboratorioqgis.qgs',
+            params: {LAYERS: capanuevanombre}
+        })
+    })
+    layer[layer.length+1] = capa;
+    addInteraction();
 
 
 
-                        /**
-                         * Currently drawed feature
-                         * @type {ol.Feature}
-                         */
-                        var sketch;
+}
+
+function agregarElemento() {
 
 
-                        /**
-                         * Element for currently drawed feature
-                         * @type {Element}
-                         */
-                        var sketchElement;
+
+}
 
 
-                        /**
-                         * handle pointer move
-                         * @param {Event} evt
-                         */
-                        var mouseMoveHandler = function(evt) {
-                          if (sketch) {
-                            var output;
-                            var geom = (sketch.getGeometry());
-
-                              output = formatLength( /** @type {ol.geom.LineString} */ (geom));
-                              console.log(output);
-
-                            sketchElement.innerHTML = output;
-                          }
-                        };
-
-                        //EL DRAMA ESTA ACA ABAJO. EL EVENTO MOUSEMOVE ES DE JQUERY. PROBABLEMENTE SI LO INCLUIS SOLUCIONAS
-                        // PROBLEMA. AHI ANDA CON CADA CLICK.
-
-                        map.addEventListener('click',mouseMoveHandler);
-                        //map.addEventListener('mousemove', mouseMoveHandler);
 
 
-                        //var typeSelect = document.getElementById('type');
-
-                        var draw; // global so we can remove it later
-                        function addInteraction() {
-                          var type = "LineString";
-                          draw = new ol.interaction.Draw({
-                            source: source,
-                            type: /** @type {ol.geom.GeometryType} */ (type)
-                          });
-                          map.addInteraction(draw);
-
-                      draw.on('drawstart',
-                          function(evt) {
-                            //set sketch
-                            sketch = evt.feature;
-                            sketchElement = document.createElement('li');
-                            var outputList = document.getElementById('measureOutput');
-
-                            if (outputList.childNodes) {
-                              outputList.insertBefore(sketchElement, outputList.firstChild);
-                            } else {
-                              outputList.appendChild(sketchElement);
-                            }
-                          }, this);
-
-                      draw.on('drawend',
-                          function(evt) {
-                            // unset sketch
-                            sketch = null;
-                            sketchElement = null;
-                          }, this);
-                    }
 
 
-                        /**
-                         * Let user change the geometry type.
-                         * @param {Event} e Change event.
-                         */
-                        /*typeSelect.onchange = function(e) {
-                          map.removeInteraction(draw);
-                          addInteraction();
-                        };*/
+
+var capanuevanombre;
+//capanuevanombre = document.getElementById("capa_nombre").value;
+ var consultar = function(coordinate){
 
 
-                        /**
-                         * format length output
-                         * @param {ol.geom.LineString} line
-                         * @return {string}
-                         */
-                        var formatLength = function(line) {
-
-                          var length = Math.round(line.getLength() * 150000);  
-                          //var length = Math.round(line.getLength() * 100) / 100;
-                          var output;
-                          if (length > 100) {
-                            output = (Math.round(length / 1000 * 100) / 100) +
-                                ' ' + 'km';
-                          } else {
-                            output = (Math.round(length * 100) / 100) +
-                                ' ' + 'm';
-                          }
-                          return output;
-                        };
-                            addInteraction();
-
-                        }
+  console.log(coordinate);
+  if(coordinate.length==2){
+    //es un punto [lon,lat]
+    var wkt='POINT('+coordinate[0]+' ' +coordinate[1]+')';
+  }else{
+    //es un poligono en la forma [ [ [lon,lat],[lon,lat],....] ]
+    var wkt = 'POLYGON((';
+    for(var i=0;i<coordinate[0].length - 1;i++){
+      wkt+=coordinate[0][i][0]+ ' ' + coordinate[0][i][1]+ ',';
+    }
+    wkt+=coordinate[0][0][0]+' '+coordinate[0][0][1]+'))'
+  }
+  console.log(wkt);
+  //+ '&'capa?='capa'+capa //
+  window.open('php/crearcapa.php?wkt='+wkt);return;
+  };
 
 
-            
-        
+   var clickEnMapa = function(evt){
+  console.log(evt.coordinate);
+
+  consultar(evt.coordinate);
+      };
+
+      //function para "cambiar" de interaction en function del value de los radios
+      var seleccionarControl = function(el){
+        if(el.value=="consulta"){
+    map.addInteraction(selectInteraction);
+    map.on('click',clickEnMapa);
+
+  }else if(el.value=="navegacion"){
+    map.removeInteraction(selectInteraction);
+    map.un('click',clickEnMapa);
+  }
+  console.log(el.value);
+      };
 
 
-    
+
+
+
+
+
+
+
+
+
 
 
