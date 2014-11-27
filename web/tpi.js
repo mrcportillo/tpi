7a4fa0d8a4e55faf0ce643bc1cf3e0c1b20d6b4c
manuel matias zubieta <manuelzubieta14@gmail.com>
2014-11-27 12:36:18 -0200

    agregado estilo de tabla y posicion de medida

--- web/tpi.js
+++ web/tpi.js
@@ -259,8 +259,8 @@ function medir(){
      */
     var mouseMoveHandler = function(evt) {
       if (sketch) {
-        var sketchElement = document.getElementById('measureOutput');;
-  
+        var sketchElement = document.getElementById('measureOutput');
+        
         var output;
         var geom = (sketch.getGeometry());
 
@@ -297,7 +297,7 @@ function medir(){
             
 
             //sketchElement = document.createElement('li');
-            sketchElement.setAttribute("class", "panel-footer");
+            
             //var outputList = document.getElementById('measureOutput');
             /*if (outputList.childNodes) {
 >>>>>>> c48ba91c5d5a09b88728bc8fde45ae4142484188
